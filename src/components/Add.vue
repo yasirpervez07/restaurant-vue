1af01b1966b38d4fabbc7133cd7895336498bd47
yasirpervez07 <yasirpervez07@gmail.com>
2022-09-02 15:04:44 +0500

    first commit

---
+++ src/components/Add.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Add.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Update.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Update.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
